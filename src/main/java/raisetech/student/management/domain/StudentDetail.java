cd825651dfdf0cc3ab3315b35512746722187c0c
Shuto Ishii <09078002436@docomo.ne.jp>
2025-01-12 17:38:20 +0900

    Initial commit

---
+++ src/main/java/raisetech/student/management/domain/StudentDetail.java
@@ -0,0 +1,21 @@
+package raisetech.student.management.domain;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentCourse;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDetail {
+
+    private Student student;
+    private List<StudentCourse> studentCourseList;
+
+}
